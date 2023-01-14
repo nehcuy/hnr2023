8e3de6bf2f82c658eafc75faad97caf228ce6de3
Yuchen <hyuchen2607@gmail.com>
2023-01-15 07:50:02 +0800

    Added tutorial page

--- src/state/tutorial.py
+++ src/state/tutorial.py
@@ -1,6 +1,7 @@
 import pygame
 import button
 import main
+import os
 
 import state.constants as c
 
@@ -15,13 +16,66 @@ class Tutorial:
         button_width = 100
         button_height = 50
         button_x = (self.screen.get_width() / 2) - (button_width / 2)
-        button_y = 45
+        button_y = self.screen.get_height() - button_height - 10
         self.back_button = button.Button(button_width, button_height, button_x, button_y, "Back")
 
+        # Image of obstacles
+        self.obstacle_image = [pygame.image.load(os.path.join(c.APP_FOLDER, "images", "obstacles", "Tree.png")),
+                                 pygame.image.load(os.path.join(c.APP_FOLDER, "images", "obstacles", "Rock.png")),
+                                 pygame.image.load(os.path.join(c.APP_FOLDER, "images", "obstacles", "Spike.png"))]
+
+    def display_upper_half(self, font):
+        # Display the tutorial text
+            text = font.render("W / Up Arrow Key: Move to lane on top", True, c.BLACK)
+            text_rect = text.get_rect()
+            text_rect.left = c.TUTORIAL_TEXT_SPACING
+            text_rect.top = c.TUTORIAL_TEXT_SPACING
+            self.screen.blit(text, text_rect)
+            text = font.render("A / Left Arrow Key: Roll", True, c.BLACK)
+            text_rect.top += c.TUTORIAL_TEXT_SPACING
+            self.screen.blit(text, text_rect)
+            text = font.render("S / Down Arrow Key: Move to lane below", True, c.BLACK)
+            text_rect.top += c.TUTORIAL_TEXT_SPACING
+            self.screen.blit(text, text_rect)
+            text = font.render("D / Right Arrow Key: Chop", True, c.BLACK)
+            text_rect.top += c.TUTORIAL_TEXT_SPACING
+            self.screen.blit(text, text_rect)
+
+            pygame.draw.rect(self.screen, c.BLACK, pygame.Rect(0, c.SCREEN_HEIGHT / 2 - c.TUTORIAL_TEXT_SPACING * 2, c.SCREEN_WIDTH, 5))
+
+    def display_lower_half(self, font):
+        tree = self.obstacle_image[0]
+        rock = self.obstacle_image[1]
+        spike = self.obstacle_image[2]
+        tree = pygame.transform.smoothscale(tree, (40, 40))
+        rock = pygame.transform.smoothscale(rock, (40, 40))
+        spike = pygame.transform.smoothscale(spike, (45, 45))
+
+        self.screen.blit(tree, (c.TUTORIAL_TEXT_SPACING, c.SCREEN_HEIGHT / 2 - c.TUTORIAL_TEXT_SPACING))
+        text = font.render("Chop the tree down or change lanes to dodge!", True, c.BLACK)
+        text_rect = text.get_rect()
+        text_rect.top = c.SCREEN_HEIGHT / 2 - c.TUTORIAL_TEXT_SPACING + 5
+        text_rect.left = c.TUTORIAL_TEXT_SPACING + 20 + c.TUTORIAL_TEXT_SPACING
+        self.screen.blit(text, text_rect)
+
+        self.screen.blit(rock, (c.TUTORIAL_TEXT_SPACING, c.SCREEN_HEIGHT / 2 + c.TUTORIAL_TEXT_SPACING))
+        text = font.render("Roll over the rock or change lanes to dodge!", True, c.BLACK)
+        text_rect.top += c.TUTORIAL_TEXT_SPACING * 2
+        self.screen.blit(text, text_rect)
+
+        self.screen.blit(spike, (c.TUTORIAL_TEXT_SPACING, c.SCREEN_HEIGHT / 2 + c.TUTORIAL_TEXT_SPACING * 3))
+        text = font.render("Escape by only changing lanes!", True, c.BLACK)
+        text_rect.top += c.TUTORIAL_TEXT_SPACING * 2
+        self.screen.blit(text, text_rect)
+        
     def run(self):
         running = True
         while running:
             self.screen.fill(c.GREY)
+            font = pygame.font.SysFont('couriernew', 25)       
+            self.display_upper_half(font)
+            self.display_lower_half(font)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
